3a8f2d507397e2d47ff88ab457c4126ea4b5b58a
Vahabov <kamilvahabov5@gmail.com>
2025-09-04 11:02:58 +0400

    version 3.0

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -292,7 +292,7 @@ export function MainScreen() {
               {isInTelegram && user ? `${user.first_name}${user.last_name ?? ''}` : 'Teejaro'}
             </span>
             <span className="text-[10px] text-foreground block text-left">
-              App Version: 1.2.0
+              App Version: 1.2.1
             </span>
           </div>
         </button>
